361b4f3fc226de09e12be2afc99ddc907db6baa5
Sachin <sachinscode24@gmail.com>
2025-08-15 09:20:45 +0600

    added problem 3 solution coment

--- problem.js
+++ problem.js
@@ -37,26 +37,28 @@
 
 // Problem-03 : FIFA Best Team Award 
 
-function bestTeam(player1, player2) {
-    // You have to write your code here
-    if (typeof player1 !== "object" || typeof player2 !== "object" || player1 === null || player2 === null) {
-        return "Invalid";
-    }
+// function bestTeam(player1, player2) {
+//     // You have to write your code here
+//     if (typeof player1 !== "object" || typeof player2 !== "object" || player1 === null || player2 === null) {
+//         return "Invalid";
+//     }
 
-    let total1 = player1.foul + player1.cardY + player1.cardR;
-    let total2 = player2.foul + player2.cardY + player2.cardR;
-    if (total1 < total2) {
-        return player1.name;
-    } else if (total2 < total1) {
-        return player2.name;
-    } else {
-        return "Tie";
-    }
+//     let total1 = player1.foul + player1.cardY + player1.cardR;
+//     let total2 = player2.foul + player2.cardY + player2.cardR;
+//     if (total1 < total2) {
+//         return player1.name;
+//     } else if (total2 < total1) {
+//         return player2.name;
+//     } else {
+//         return "Tie";
+//     }
 
-}
+// }
 
-let fearTeam = bestTeam(
-   { name: "Germany", foul: 10, cardY: 1, cardR: 1 },"France"
-);
+// let fearTeam = bestTeam(
+//    { name: "Germany", foul: 10, cardY: 1, cardR: 1 },"France"
+// );
 
-console.log(fearTeam);
+// console.log(fearTeam);
+
+
